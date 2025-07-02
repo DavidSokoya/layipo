d596c5bfe71f1c11a0187deb81b119b87757b9a9
David Sokoya <sokoya08@gmail.com>
2025-07-02 23:27:38 +0000

    change the width of the elevate to 12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,7 @@ const HomePageHeader = () => {
             </h1>
         </Link>
       </div>
-      <Image src="/logos/elevate_lo.png" alt="Elevate Logo" width={75} height={24} className="object-contain" />
+      <Image src="/logos/elevate_lo.png" alt="Elevate Logo" width={12} height={24} className="object-contain" />
     </div>
   );
 };
